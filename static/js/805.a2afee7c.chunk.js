c65e488e95cf2ea226df5a8c97b109592768c8a1
Valerii-Postrybailo <99152023+Valerii-Postrybailo@users.noreply.github.com>
2022-12-09 09:41:13 +0000

    Deploying to gh-pages from @ Valerii-Postrybailo/ReactPhonebook@23a5520bc848683e0c1c979a9574dc81a78644e9 🚀

---
+++ static/js/805.a2afee7c.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkReactPhonebook=self.webpackChunkReactPhonebook||[]).push([[805],{9805:function(e,n,o){o.r(n),o.d(n,{default:function(){return c}});var t=o(1087),r=o(3329);function c(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("h2",{children:"404 Page not found"}),(0,r.jsx)("p",{children:(0,r.jsx)(t.rU,{to:"/",children:"Go to the home page"})})]})}}}]);
+//# sourceMappingURL=805.a2afee7c.chunk.js.map
